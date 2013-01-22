269811a7e74b982ecd8c7efac313f40283274e8d
fritzvd <fritz.vandeventer@nelen-schuurmans.nl>
2013-01-22 11:00:34 +0100

    added local settings to gitignore

--- app/localsettings.js
+++
@@ -1,5 +0,0 @@
-var local_settings = {
-    api_parameters: 'http://localhost:3000/api/v1/parameters',
-    api_locations: 'http://localhost:3000/api/v1/locations',
-    api_filters: 'http://localhost:3000/api/v1/filters',
-}
