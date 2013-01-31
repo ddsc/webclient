6ba19838aa65ca5b8ffd0196ec0a4c554ee4bcaa
Gijs Nijholt <gijs.nijholt@gmail.com>
2013-01-31 12:09:45 +0100

    Check results in parse()

--- app/scripts/lizard/lizard.collections.js
+++ app/scripts/lizard/lizard.collections.js
@@ -6,29 +6,47 @@ Lizard.collections = {};
 
 Lizard.collections.Filter = Backbone.Collection.extend({
   initialize: function() {
-    console.log('Filter initializing');
+    console.log('Filter collection initializing');
   },
   url: local_settings.filters_url,
-  model: Lizard.models.Filter
+  model: Lizard.models.Filter,
+  parse: function(res, xhr) {
+    if(res.results) {
+      return res.results;
+    } else {
+      return res;
+    }
+  }  
 });
 
 Lizard.collections.Location = Backbone.Collection.extend({
   initialize: function() {
-    console.log('Location initializing');
+    console.log('Location collection initializing');
   },
   url: local_settings.locations_url,
   model: Lizard.models.Location,
   parse: function(res, xhr) {
-    return res.results;
+    if(res.results) {
+      return res.results;
+    } else {
+      return res;
+    }
   }
 });
 
 Lizard.collections.Parameter = Backbone.Collection.extend({
   initialize: function() {
-    console.log('Parametern initializing');
+    console.log('Parameter collection initializing');
   },
   url: local_settings.parameters_url,
-  model: Lizard.models.Parameter
+  model: Lizard.models.Parameter,
+  parse: function(res, xhr) {
+    if(res.results) {
+      return res.results;
+    } else {
+      return res;
+    }
+  }  
 });
 
 Lizard.collections.Collage = Backbone.Collection.extend();
