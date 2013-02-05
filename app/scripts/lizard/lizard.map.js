7bbbaf9350c0e54136945abddef4f814a2eeca2f
fritzvd <fritz.vandeventer@nelen-schuurmans.nl>
2013-02-05 13:56:12 +0100

    graph working without real data and hacky

--- app/scripts/lizard/lizard.map.js
+++ app/scripts/lizard/lizard.map.js
@@ -116,20 +116,11 @@ Lizard.Map.IconCollectionView = Backbone.Marionette.CollectionView.extend({
 });
 
 
-Lizard.Map.Timeseries = Backbone.Marionette.ItemView.extend({
-  template: '#location-modal-template',
-  collection: new Lizard.collections.Timeserie(),
-  onShow: function (){
-    console.log(this.collection.fetch());
-  }
-});
-
-Lizard.views.ModalGraph = Backbone.Marionette.CollectionView.extend({
+Lizard.views.ModalGraph = Backbone.Marionette.ItemView.extend({
     template: '#location-modal-template',
-    initialize: function(){
-    },
     onShow: function(){
-      var chart = new Highcharts.Chart({
+      var chart;
+      chart = new Highcharts.Chart({
                 chart: {
                     renderTo:'chartarea',
                     type: 'line',
@@ -170,8 +161,20 @@ Lizard.views.ModalGraph = Backbone.Marionette.CollectionView.extend({
                 y: 100,
                 borderWidth: 0
             },
-            series: []
-        });
+            series: [{
+                name: 'Tokyo',
+                data: [7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6]
+            }, {
+                name: 'New York',
+                data: [-0.2, 0.8, 5.7, 11.3, 17.0, 22.0, 24.8, 24.1, 20.1, 14.1, 8.6, 2.5]
+            }, {
+                name: 'Berlin',
+                data: [-0.9, 0.6, 3.5, 8.4, 13.5, 17.0, 18.6, 17.9, 14.3, 9.0, 3.9, 1.0]
+            }, {
+                name: 'London',
+                data: [3.9, 4.2, 5.7, 8.5, 11.9, 15.2, 17.0, 16.6, 14.2, 10.3, 6.6, 4.8]
+            }]
+        })
     }
 })
 
