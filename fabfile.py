091be1408ad932cd6e025fe1e0bbff8fe5f1e3a5
fritzvd <fritz.vandeventer@nelen-schuurmans.nl>
2013-02-05 11:29:35 +0100

    added fabfile fmbo deploy

--- fabfile.py
+++ fabfile.py
@@ -1,8 +1,15 @@
 from sitesetup.fab.config import init_file
-from sitesetup.fab.config import instances
-from sitesetup.fab.detail import create_individual_database
-from sitesetup.fab.tasks import *
+from sitesetup.fab.tasks import 
 
 # Most settings can be configured in fabfile.cfg
 init_file('fabfile.cfg')
 
+@task
+def init():
+    detail.initial_create_srv_dir()
+    detail.switch_and_buildout()
+    detail.initial_nginx_symlinks()
+
+@task
+def update():
+    detail.switch_and_buildout()
