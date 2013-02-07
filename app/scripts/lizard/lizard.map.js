efc8b32f82c99e303eb4cf47c08923bf35b8b55c
Gijs Nijholt <gijs.nijholt@gmail.com>
2013-02-07 17:32:47 +0100

    Fritz: you fixed this already, just didnt push it... so I re-do it now

--- app/scripts/lizard/lizard.map.js
+++ app/scripts/lizard/lizard.map.js
@@ -304,7 +304,7 @@ Lizard.Map.LeafletView = Backbone.Marionette.ItemView.extend({
   updateInfo: Lizard.Utils.Map.updateInfo,
   onDomRefresh: function(){
     // Best moment to initialize Leaflet and other DOM-dependent stuff
-    Rethis.mapCanvas = L.map('map', { layers: [this.cloudmade], center: new L.LatLng(52.12, 5.2), zoom: 7});
+    this.mapCanvas = L.map('map', { layers: [this.cloudmade], center: new L.LatLng(52.12, 5.2), zoom: 7});
     this.markers = new L.MarkerClusterGroup({
       spiderfyOnMaxZoom: true,
       showCoverageOnHover: false,
