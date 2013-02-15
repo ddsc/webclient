b1a80327219e62ffbb3a156d492e80789f0e97b1
Gijs Nijholt <gijs.nijholt@gmail.com>
2013-02-15 16:39:18 +0100

    Sorry!

--- app/settings.js
+++ app/settings.js
@@ -1,6 +1,6 @@
 var extra = ''; //page_size=0
 
-var domain = (domain ? domain : 'http://api.dijkdata.nl/api/v0/');
+var domain = (domain ? domain : 'http://test.api.dijkdata.nl/api/v0/');
 
 var settings = {
     parameters_url: domain + 'parameters/' + extra,
