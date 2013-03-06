6a665fe8fafc317478e94642de13559af24f3c62
Gijs Nijholt <gijs.nijholt@gmail.com>
2013-03-06 17:06:30 +0100

    WIP

--- app/scripts/lizard/lizard.collections.js
+++ app/scripts/lizard/lizard.collections.js
@@ -60,6 +60,17 @@ Lizard.Collections.Collage = Backbone.Collection.extend({
 
 
 /* TIMESERIES COLLECTIONS */
+
+Lizard.Collections.InfiniteTimeseries = Backbone.Collection.extend({
+  url: function() {
+    return 'http://api.dijkdata.nl/api/v0/timeseries/?page=' + this.page;
+  },
+  parse: function(resp, xhr) {
+    return resp.results;
+  },
+  page: 1
+});
+
 Lizard.Collections.Timeseries = Backbone.Collection.extend({
   initialize: function(){
    this.fetch({
@@ -87,3 +98,4 @@ filterCollection = new Lizard.Collections.Filter();
 locationCollection = new Lizard.Collections.Location();
 parameterCollection = new Lizard.Collections.Parameter();
 timeseriesCollection = new Lizard.Collections.Timeseries();
+infinitetimeseries = new Lizard.Collections.InfiniteTimeseries();
