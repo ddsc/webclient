a7f0f337695e7709e0c79b288528fa8e8f58965a
Gijs Nijholt <gijs.nijholt@gmail.com>
2013-03-19 12:09:20 +0100

    Removed cruft

--- app/scripts/lizard/widgets/TemplateWidget.js
+++ app/scripts/lizard/widgets/TemplateWidget.js
@@ -1,10 +1,7 @@
 Lizard.Widgets.TemplateWidget = Marionette.ItemView.extend({
   getTemplate: function() {
     return this.model.get('template');
-  }//,
-  //render: function() {
-  //  debugger
-  //}
+  }
 });
 
 WIDGET_CLASSES['template'] = Lizard.Widgets.TemplateWidget;
