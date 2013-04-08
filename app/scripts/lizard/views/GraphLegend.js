06e41f409e9a39d07cdc390011b9d9fd6042b997
Gijs Nijholt <gijs.nijholt@gmail.com>
2013-04-08 15:39:17 +0200

    Working on Graph legend view

--- app/scripts/lizard/views/GraphLegend.js
+++ app/scripts/lizard/views/GraphLegend.js
@@ -1,17 +1,43 @@
-GraphLegendItemView = Backbone.Marionette.ItemView.extend({
-  initialize: function() {
-    console.log('GraphLegendItemView initializing');
-  },
-  template: '#graphs-legend-template',
-  events: {
-    'drop': 'handleDrop'
-  },
-  handleDrop: function() {
-    alert('Dropped!');
-  }
+NoItemsView = Backbone.Marionette.ItemView.extend({
+  template: "#show-no-items-message-template"
 });
 
-GraphLegendCollection = Backbone.Marionette.CollectionView.extend({
+GraphLegendItemView = Backbone.Marionette.ItemView.extend({
+  template: '#graphs-legend-template'
+});
+
+GraphLegendCollectionView = Backbone.Marionette.CollectionView.extend({
   itemView: GraphLegendItemView,
-  emptyView: Lizard.Map.NoItemsView
+  emptyView: NoItemsView,
+  initialize: function() {
+    console.log('initialize()');
+    // console.log($(this.el).parent('.row-fluid'));
+    // var g = this.$el.parent('graph').initFlotGraph();
+    // _.bind(this.collection);
+  },
+  onShow: function(e) {
+    var that = this;
+    this.$el.on('dragover dragenter', false);
+    this.$el.on('drop', function(e) {
+      e.preventDefault();
+      console.log('e', e.originalEvent.target);
+      var $target = $(e.target);
+      var graph = new Lizard.Models.Graph();
+      var uuid = e.originalEvent.dataTransfer.getData('Text');
+      var timeserie = timeseriesCollection.get(uuid);
+      that.collection.add(timeserie);
+
+
+      $target.parent().removeClass("empty");
+      // only fire for nearest .graph-drop parent (in case there is already a graph in the element)
+      var $graph = $target;
+      if (!$graph.hasClass('graph-drop')) {
+          $graph = $target.parent('.graph-drop');
+      }
+      $graph.loadPlotData(settings.timeseries_url + 'events/' + uuid);
+
+      // var $graph = $('#drop-one .graph-drop');
+      // $graph.loadPlotData(settings.timeseries_url + 'events/' + uuid);
+    });
+  }
 });
