a0833ba852cf1cd0059b5c23bd4041b5ee564d74
ejnens <erikjan.vos@nelen-schuurmans.nl>
2013-04-16 16:50:56 +0200

    disabled this for now

--- app/scripts/lizard/models/Graph.js
+++ app/scripts/lizard/models/Graph.js
@@ -2,7 +2,7 @@ Lizard.Models.Graph = Backbone.Model.extend({
     initialize: function () {
         var graphItems = new Lizard.Collections.GraphItem();
         this.set('graphItems', graphItems);
-        graphItems.on('add remove reset', this.change, this);
+        //graphItems.on('add remove reset', this.change, this);
     },
     change: function (e) {
         this.set('hasTwoItems', this.get('graphItems').length == 2);
