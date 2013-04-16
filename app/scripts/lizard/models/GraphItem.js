be4c3bc050091e0efe50a2414bf2604ef674fa99
Gijs Nijholt <gijs.nijholt@gmail.com>
2013-04-16 10:59:07 +0200

    Pick colors from entirety of Colorbrewers spectrum

--- app/scripts/lizard/models/GraphItem.js
+++ app/scripts/lizard/models/GraphItem.js
@@ -1,11 +1,21 @@
 Lizard.Models.GraphItem = Backbone.Model.extend({
     defaults: {
         timeseries: null,
-        color: '#ccc'
+        color: '#ccc' // Grey by default
     },
     initialize: function() {
+		
+		// Get a random color set from Colorbrewer.js
+		var colorSet = colorbrewer[Object.keys(colorbrewer)[Math.floor(Object.keys(colorbrewer).length * Math.random())]];
+		
+		// Get a random array from colorSet
+		var colorArray = colorSet[Object.keys(colorSet)[Math.floor(Object.keys(colorSet).length * Math.random())]];
+		
+		// Get a random color from colorArray
+		var color = colorArray[(Math.floor(Math.random()*colorArray.length-1)+1)];
+
         this.set({
-            'color': colorbrewer.Set3[9][Math.floor(Math.random()*8+1)]
+            'color': color
         });
     }
 });
