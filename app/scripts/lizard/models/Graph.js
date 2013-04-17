7cee973ce70188dd3845c30dbd6dc4c537537238
ejnens <erikjan.vos@nelen-schuurmans.nl>
2013-04-17 09:54:21 +0200

    properly cleanup event listener

--- app/scripts/lizard/models/Graph.js
+++ app/scripts/lizard/models/Graph.js
@@ -2,9 +2,13 @@ Lizard.Models.Graph = Backbone.Model.extend({
     initialize: function () {
         var graphItems = new Lizard.Collections.GraphItem();
         this.set('graphItems', graphItems);
-        graphItems.on('add remove reset', this.change, this);
+        graphItems.on('add remove reset', this.graphItemsChange, this);
     },
-    change: function (e) {
+    teardown: function () {
+        var graphItems = this.get('graphItems');
+        graphItems.off('add remove reset', this.graphItemsChange, this);
+    },
+    graphItemsChange: function (e) {
         this.set('hasTwoItems', this.get('graphItems').length == 2);
     },
     defaults: {
