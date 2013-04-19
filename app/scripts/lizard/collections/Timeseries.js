094e334baaec918f597b2e2418731d2f53be7f66
Gijs Nijholt <gijs.nijholt@gmail.com>
2013-04-19 10:59:02 +0200

    Prevent fetching timeseries in graph screen when client is mobile…

--- app/scripts/lizard/collections/Timeseries.js
+++ app/scripts/lizard/collections/Timeseries.js
@@ -12,9 +12,14 @@ Lizard.Collections.InfiniteTimeseries = Backbone.Collection.extend({
 
 Lizard.Collections.Timeseries = Backbone.Collection.extend({
   initialize: function(){
-   this.fetch({
-      cache: false
-    });
+    var self = this;
+    if( /Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent) ) {
+      console.log('Not fetching timeseries since we seem to be on a mobile device');
+    } else {
+      self.fetch({
+        cache: true
+      });
+    }
   },
   comparator: function(timeserie) {
     return timeserie.get('name');
