bb7e20f8b01e6797923faf5310ef78277c33877a
Gijs Nijholt <gijs.nijholt@gmail.com>
2013-04-26 15:40:58 +0200

    Placeholder (animated gif) on empty graphs

--- app/scripts/lizard/views/Graph.js
+++ app/scripts/lizard/views/Graph.js
@@ -4,16 +4,28 @@ Lizard.Views.Graph = Backbone.Marionette.View.extend({
     model: Lizard.Models.Graph,
     initialize: function (options) {
         this.account = options.account;
+
+    },
+    showHideTemplate: function(e) {
+        console.log('changed:', this.$el);
+        if (this.model.get('graphItems').length === 0) {
+            this.$el.find('.overlayer').show();
+        } else {
+            this.$el.find('.overlayer').hide();
+        }
     },
     onShow: function (e) {
         var plot = this.$el.initializePlot();
         plot.observeCollection(this.model.get('graphItems'));
         plot.observeInitialPeriod(this.account);
         this.plot = plot;
+        this.$el.append('<div class="overlayer hidden-tablet hidden-phone"><img src="images/drag_animation.gif"/></div>');
+        this.model.get('graphItems').on('add remove reset', this.showHideTemplate, this);
     },
     onClose: function (e) {
         if (this.plot) {
             this.plot.shutdown();
         }
+        this.model.get('graphItems').off('add remove reset', this.showHideTemplate, this);
     }
 });
