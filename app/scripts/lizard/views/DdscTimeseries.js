7122211687f5e0c89969b63f5c9b0200a930daef
fritzvd <fritz.vandeventer@nelen-schuurmans.nl>
2013-04-29 13:34:40 +0200

    fixed timeseries in popups on maps

--- app/scripts/lizard/views/DdscTimeseries.js
+++ app/scripts/lizard/views/DdscTimeseries.js
@@ -16,35 +16,11 @@ function format_value(value) {
 }
 
 Lizard.Map.LocationModalTimeseries = Backbone.Marionette.Layout.extend({
-  // template: function(model){
-    // return _.template($('#location-modal-timeserie').html(), {
-      // name: model.name,
-      // uuid: model.url.split("eries/")[1].split("/")[0],
-      // events: model.events,
-      // latest_value: model.latest_value,
-    // }, {variable: 'timeserie'});
-  // },
   template: '#location-modal-timeserie',
-  //uuid: null,
   initialize: function(options) {
-    //this.uuid = this.model.url.split("eries/")[1].split("/")[0];
   },
   events: {
     'click .graph-this': "drawGraph"
-    // 'click .fav': 'toggleFavorite'
-  },
-  toggleFavorite: function() {
-    // var favorite = this.model.get('favorite');
-    // if(favorite) {
-      // this.model.set({"favorite": false});
-      // this.$el.find('i.icon-star').removeClass('icon-star').addClass('icon-star-empty');
-    // } else {
-      // this.model.set({"favorite": true});
-      // this.$el.find('i.icon-star-empty').removeClass('icon-star-empty').addClass('icon-star');
-    // }
-    //uuid = this.uuid;
-    //type = 'timeseries';
-    //Lizard.Utils.Favorites.toggleSelected(this.model);
   },
   // One Timeserie has many Events. An Events list is only
   // loaded when it is explcitly chosen, with caching.
@@ -114,29 +90,11 @@ Lizard.Views.LocationModalPopup = Backbone.Marionette.Layout.extend({
         });
         this.timeseriesRegion.show(timeseriesView);
     },
-  // itemView: Lizard.Map.LocationModalTimeseries,
-  // onRender: function (model){
-    // $('#location-modal-label').html(this.location);
-    // this.emptyGraph();
-  // },
-  // emptyGraph: function(){
-    // var graph_elements = $('.flot-graph');
-//
-    // _.each(graph_elements, function(graph_element) {
-      // var plot = $(graph_elements).data('plot');
-      // if (plot) {
-          // plot.removeAllDataUrls();
-        // }
-    // });
-  // }
 });
 
 Lizard.Views.LocationPopupItem = Backbone.Marionette.ItemView.extend({
   template: '#location-popup-item',
   tagName: 'li',
-  // initialize: function (options) {
-    // this.otherTimeseries = options.otherTimeseries;
-  // },
   events: {
     'click .popup-toggle' : "openModal",
     'click .icon-comment' : 'openAnnotation'
@@ -153,19 +111,7 @@ Lizard.Views.LocationPopupItem = Backbone.Marionette.ItemView.extend({
     modalView.$el.find('.modal').on('hide', function () {
         Lizard.App.hidden.close();
     });
-
-    // Lizard.mapView.modalitems.show(modalView.render());
-    // this.uuid = this.model.attributes.uuid;
-    // $('#location-modal').modal();
-    // $('#location-modal').on('show', this.showGraph, this);
-  },
-  // showGraph: function(e){
-      // var data_url = $(e.target).data('url');
-      // console.log(data_url, $('#modal-graph-wrapper'));
-      // $('#modal-graph-wrapper').removeClass('hidden');
-      // var flot_div = $('#modal-graph-wrapper').find('.flot-graph');
-      // $(flot_div).loadPlotData(data_url + '?eventsformat=flot');
-  // },
+  }
 });
 
 // Modal view that opens when clicking on a location
@@ -175,23 +121,12 @@ Lizard.Views.LocationPopup = Backbone.Marionette.CollectionView.extend({
 });
 
 Lizard.geo.Popups.DdscTimeseries = {
-  // modalInfo: function (e){
-    // var marker = e.target;
-    // var model = marker.valueOf().options.bbModel;
-    // $('#modal-graph-wrapper').find('.flot-graph').empty();
-    // modalView = new Lizard.Map.ModalTimeseriesView();
-    // modalView.locationuuid = model.attributes.uuid;
-    // modalView.location = model.attributes.name;
-    // timeseriesCollection.reset();
-    // timeseriesCollection.fetch({cache: true});
-    // Lizard.mapView.modalitems.show(modalView.render());
-    // $('#location-modal').modal();
-  // },
   getPopupContent: function (location, $elem) {
     var url = settings.timeseries_url + '&location=' + location.get('uuid');
-    var timeseriesCollection = new Lizard.Collections.Timeseries();
-    timeseriesCollection.url = url;
-    timeseriesCollection.fetch().done(function (collection, response) {
+    var tsCollection = new Lizard.Collections.Timeseries();
+    tsCollection.url = url;
+    debugger
+    tsCollection.fetch().done(function (collection, response) {
         var popupView = new Lizard.Views.LocationPopup({
             collection: collection
         });
@@ -199,26 +134,5 @@ Lizard.geo.Popups.DdscTimeseries = {
         var popupContent = popupView.render().el;
         $elem.append(popupContent);
     });
-
-    // popupView.locationuuid = model.attributes.uuid;
-    // popupView.location = model.attributes.name;
-    // timeseriesCollection.reset();
-    // timeseriesCollection.fetch();
-  },
-  // updateInfo: function (e) {
-    // var marker = e.target;
-    // console.log(e);
-    // // props = marker.valueOf().options;
-    // // e.layer._map._controlContainer.innerHTML = '<h4>Datapunt</h4>' + (props ?
-    // //         '<b>' + props.name + '</b><br>' +
-    // //         'Punt: ' + props.code
-    // //         : 'Zweef over de punten');
-  // },
-  // selectforCollage: function(e) {
-    // var marker = e.target;
-    // var properties = marker.valueOf().options;
-    // var wsitem = properties.bbModel;
-    // wsitem.set({title: wsitem.attributes.name});
-    // Collage.add(wsitem);
-  // }
+  }
 };
