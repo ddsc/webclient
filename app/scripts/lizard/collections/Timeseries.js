1e122a47bcc98829f299dd1271b20f213a293c2f
ejnens <erikjan.vos@nelen-schuurmans.nl>
2013-05-17 13:46:19 +0200

    fix timeseries url

--- app/scripts/lizard/collections/Timeseries.js
+++ app/scripts/lizard/collections/Timeseries.js
@@ -2,7 +2,7 @@
 
 Lizard.Collections.InfiniteTimeseries = Backbone.Collection.extend({
   url: function() {
-    return settings.timeseries_url + '?order=name&value_type=0,1&page_size=' + (this.pageSize * this.page) + '&page=1&name=' + this.name;
+    return settings.timeseries_url + '&order=name&value_type=0,1&page_size=' + (this.pageSize * this.page) + '&page=1&name=' + this.name;
   },
   // COMPARATOR DOES NOT WORK WELL HERE, MUST HAVE PROPER ORDERING ON SERVER RETURN?
   parse: function(resp, xhr) {
