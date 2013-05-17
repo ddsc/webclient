f064734254300c0e2fe4775baf9621fef507a8b3
ejnens <erikjan.vos@nelen-schuurmans.nl>
2013-05-17 13:36:15 +0200

    fix timeseries url

--- app/scripts/settings.js
+++ app/scripts/settings.js
@@ -9,7 +9,7 @@ var settings = {
     locations_url: domain + 'locations/?has_geometry=true&for_map=true&page_size=5000',
     wms_proxy_base_url: domain + 'proxy/?',
     filters_url: domain +'logicalgroups/' + extra,
-    timeseries_url: domain + 'timeseries/?page_size=100',
+    timeseries_url: domain + 'timeseries/',
     alarms_url: domain + 'alarms/',
     collages_url: domain + 'collages/?page_size=100',
     workspace_url: domain + 'workspaces/?page_size=100',
