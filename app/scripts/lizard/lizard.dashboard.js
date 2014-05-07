9ac924346bac050dcfce4fb186c188c507fd10e6
fritzvd <f.vandeventer@gmail.com>
2014-05-07 16:28:09 +0200

    :facepunch: got the dmc to work 'sort of'

--- app/scripts/lizard/lizard.dashboard.js
+++ app/scripts/lizard/lizard.dashboard.js
@@ -27,17 +27,26 @@ Lizard.Dashboard.dashboard = function(){
   var widgetcollectionview = new Lizard.Views.WidgetCollection();
 
 
+  // var timeseries = [
+  //   "https://api.ddsc.nl/api/v1/events/9DFDC3F7-88F7-4FF0-A671-B4094AC12B7A?eventsformat=flot&ignore_rejected=true&width=645&height=300",
+  //   "https://api.ddsc.nl/api/v1/events/49108710-6C58-4E31-AC2D-3C4933040439?eventsformat=flot&ignore_rejected=true&width=645&height=300",
+  //   // "https://api.ddsc.nl/api/v1/events/DD7A4DB5-4261-4AC4-9583-0572E7F39F66?eventsformat=flot&ignore_rejected=true&width=645&height=300",
+  //   // "https://api.ddsc.nl/api/v1/events/D856946F-FEB6-4536-A878-AF55C32CD96C?eventsformat=flot&ignore_rejected=true&width=645&height=300",
+  //   "https://api.ddsc.nl/api/v1/events/03073703-3509-40F2-BA89-D85361C4021B?eventsformat=flot&ignore_rejected=true&width=645&height=300",
+  //   "https://api.ddsc.nl/api/v1/events/29119285-2428-4863-B630-9B5AA095DE13?eventsformat=flot&ignore_rejected=true&width=645&height=300"
+  // ];
+
   var timeseries = [
-    "http://api.ddsc.nl/api/v1/events/9DFDC3F7-88F7-4FF0-A671-B4094AC12B7A",
-    "http://api.ddsc.nl/api/v1/events/49108710-6C58-4E31-AC2D-3C4933040439",
-    "http://api.ddsc.nl/api/v1/events/DD7A4DB5-4261-4AC4-9583-0572E7F39F66",
-    "http://api.ddsc.nl/api/v1/events/D856946F-FEB6-4536-A878-AF55C32CD96C",
-    "http://api.ddsc.nl/api/v1/events/03073703-3509-40F2-BA89-D85361C4021B",
-    "http://api.ddsc.nl/api/v1/events/29119285-2428-4863-B630-9B5AA095DE13"
+    "https://api.ddsc.nl/api/v1/timeseries/9DFDC3F7-88F7-4FF0-A671-B4094AC12B7A",
+    "https://api.ddsc.nl/api/v1/timeseries/49108710-6C58-4E31-AC2D-3C4933040439",
+    // "https://api.ddsc.nl/api/v1/events/DD7A4DB5-4261-4AC4-9583-0572E7F39F66",
+    // "https://api.ddsc.nl/api/v1/events/D856946F-FEB6-4536-A878-AF55C32CD96C",
+    "https://api.ddsc.nl/api/v1/timeseries/03073703-3509-40F2-BA89-D85361C4021B",
+    "https://api.ddsc.nl/api/v1/timeseries/29119285-2428-4863-B630-9B5AA095DE13"
   ];
 
   var dmcCollection = Backbone.Collection.extend({
-    url: "http://api.ddsc.nl/api/v1/timeseries/?logicalgroup=7",
+    url: "https://api.ddsc.nl/api/v1/timeseries/?name=omzd_dmc",
     model: Lizard.Models.Timeserie
   });
 
@@ -46,10 +55,9 @@ Lizard.Dashboard.dashboard = function(){
   dmc.fetch({
     cache: false,
     success: function(collection) {
-
       var values = new Backbone.Model({
-        debiet_l: collection.get('D856946F-FEB6-4536-A878-AF55C32CD96C').get('latest_value'),
-        debiet_r: collection.get('DD7A4DB5-4261-4AC4-9583-0572E7F39F66').get('latest_value'),
+        // debiet_l: collection.get('D856946F-FEB6-4536-A878-AF55C32CD96C').get('latest_value'),
+        // debiet_r: collection.get('DD7A4DB5-4261-4AC4-9583-0572E7F39F66').get('latest_value'),
         temp_l: collection.get('29119285-2428-4863-B630-9B5AA095DE13').get('latest_value'),
         temp_r: collection.get('03073703-3509-40F2-BA89-D85361C4021B').get('latest_value'),
         waterst_l: collection.get('49108710-6C58-4E31-AC2D-3C4933040439').get('latest_value'),
@@ -64,10 +72,10 @@ Lizard.Dashboard.dashboard = function(){
         new Lizard.Models.Widget(_.extend({col:2,row:3,size_x:7,size_y:3,gaugeId:2,type:'template', template:'#dmc-overview'},values.attributes)),
 
         new Lizard.Models.Widget({col:3,row:1,size_x:3,size_y:2,gaugeId:4,type:'template', template:'#dashboard-logo'}),
-        new Lizard.Models.Widget({col:3,row:3,size_x:3,size_y:2,gaugeId:5,title:'Debiet links',label:'liter',
-          value: values.get('debiet_l') , max: 5000}),
-        new Lizard.Models.Widget({col:3,row:5,size_x:3,size_y:2,gaugeId:7,title:'Debiet rechts',label:'liter',
-          value: values.get('debiet_r'), max: 5000}),
+        // new Lizard.Models.Widget({col:3,row:3,size_x:3,size_y:2,gaugeId:5,title:'Debiet links',label:'liter',
+        //   value: values.get('debiet_l') , max: 5000}),
+        // new Lizard.Models.Widget({col:3,row:5,size_x:3,size_y:2,gaugeId:7,title:'Debiet rechts',label:'liter',
+        //   value: values.get('debiet_r'), max: 5000}),
         new Lizard.Models.Widget({col:3,row:7,size_x:3,size_y:1,gaugeId:8,type:'template', template:'#settings-button'}),
         new Lizard.Models.Widget({col:3,row:8,size_x:7,size_y:4,gaugeId:9,type:'template', template:'#image-dmc'})
       ]);
