5be9dae04e50eb81ce444742cd463156fef587d0
Carsten Byrman <carsten.byrman@nelen-schuurmans.nl>
2014-06-05 16:59:22 +0200

    Improve DMC dashboard.

--- app/scripts/lizard/lizard.dashboard.js
+++ app/scripts/lizard/lizard.dashboard.js
@@ -27,12 +27,13 @@ Lizard.Dashboard.dashboard = function(){
   var widgetcollectionview = new Lizard.Views.WidgetCollection();
 
   var timeseries = [
-    "https://api.ddsc.nl/api/v1/timeseries/9DFDC3F7-88F7-4FF0-A671-B4094AC12B7A",
-    "https://api.ddsc.nl/api/v1/timeseries/49108710-6C58-4E31-AC2D-3C4933040439",
-    // "https://api.ddsc.nl/api/v1/events/DD7A4DB5-4261-4AC4-9583-0572E7F39F66",
-    // "https://api.ddsc.nl/api/v1/events/D856946F-FEB6-4536-A878-AF55C32CD96C",
-    "https://api.ddsc.nl/api/v1/timeseries/03073703-3509-40F2-BA89-D85361C4021B",
-    "https://api.ddsc.nl/api/v1/timeseries/29119285-2428-4863-B630-9B5AA095DE13"
+    "https://api.ddsc.nl/api/v1/timeseries/9DFDC3F7-88F7-4FF0-A671-B4094AC12B7A",  // Waterkolom dijk rechts in mNAP
+    "https://api.ddsc.nl/api/v1/timeseries/49108710-6C58-4E31-AC2D-3C4933040439",  // Waterkolom dijk links in mNAP
+    "https://api.ddsc.nl/api/v1/timeseries/03073703-3509-40F2-BA89-D85361C4021B",  // Temperatuur Dijk Rechts
+    "https://api.ddsc.nl/api/v1/timeseries/29119285-2428-4863-B630-9B5AA095DE13",  // Temperatuur Dijk Links
+    "https://api.ddsc.nl/api/v1/timeseries/DD7A4DB5-4261-4AC4-9583-0572E7F39F66",  // Actueel Debiet Dijk Rechts
+    "https://api.ddsc.nl/api/v1/timeseries/D856946F-FEB6-4536-A878-AF55C32CD96C",  // Actueel Debiet Dijk Links
+    "https://api.ddsc.nl/api/v1/timeseries/5C8D7780-14C8-4237-9BCA-73849E7990B7"   // Buitendruk in kPa
   ];
 
   var dmcCollection = Backbone.Collection.extend({
@@ -62,9 +63,9 @@ Lizard.Dashboard.dashboard = function(){
         new Lizard.Models.Widget(_.extend({col:2,row:3,size_x:7,size_y:3,gaugeId:2,type:'template', template:'#dmc-overview'},values.attributes)),
 
         new Lizard.Models.Widget({col:3,row:1,size_x:3,size_y:2,gaugeId:4,type:'template', template:'#dashboard-logo'}),
-        new Lizard.Models.Widget({col:3,row:3,size_x:3,size_y:2,gaugeId:5,title:'Debiet links',label:'liter',
+        new Lizard.Models.Widget({col:3,row:3,size_x:3,size_y:2,gaugeId:5,title:'Debiet links',label:'liter/uur',
           value: values.get('debiet_l') , max: 5000}),
-        new Lizard.Models.Widget({col:3,row:5,size_x:3,size_y:2,gaugeId:7,title:'Debiet rechts',label:'liter',
+        new Lizard.Models.Widget({col:3,row:5,size_x:3,size_y:2,gaugeId:7,title:'Debiet rechts',label:'liter/uur',
           value: values.get('debiet_r'), max: 5000}),
         new Lizard.Models.Widget({col:3,row:7,size_x:3,size_y:1,gaugeId:8,type:'template', template:'#settings-button'}),
         new Lizard.Models.Widget({col:3,row:8,size_x:7,size_y:4,gaugeId:9,type:'template', template:'#image-dmc'})
