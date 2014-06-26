ddsc/webclient

commit 96a6342f30736900081e9944265d8749fde77d39

fullscreen fix with smaller widths

app/scripts/utilities.js
@@ -41,6 +41,10 @@ window.toggleFullScreen = function () {
         fullScreenMap.parentNode.removeChild(fullScreenMap); //remove()
         var locationSearch = document.getElementById('geocoderRegion');
         locationSearch.style.cssText = "";
+
+        var header = document.getElementById('header');
+        header.classList.add('fullscreen');
+
         Lizard.mapView.fullScreenRegion.close();
         var lonlatzoom = window.location.hash.split('#map/')[1];
         if (lonlatzoom !== undefined) {
@@ -68,6 +72,9 @@ window.toggleFullScreen = function () {
             var container = document.getElementById('content').parentNode;
             container.parentNode.insertBefore(fullScreenMap, container);
         }
+
+        var header = document.getElementById('header');
+        header.classList.remove('fullscreen');
         fullScreenMap.style.cssText = "position: absolute; width: 100%; height: calc(100% - 40px); top: 40px;";
         var locationSearch = document.getElementById('geocoderRegion');
         locationSearch.style.cssText = "margin-top: calc(75%);"

app/styles/main.css
@@ -45,7 +45,7 @@ iframe {
   width: 100%;
 }
 
-#header {
+#header.fullscreen {
     height: 40px !important;
     overflow: hidden;
     display:block ;

commit c74abadaf04103e7ebb8ce1f61c048f3db510048

Fix full-screen bug introduced in 96a6342f30736900081e9944265d8749fde77d39.

app/scripts/lizard/lizard.map.js
@@ -134,6 +134,7 @@ Lizard.Map.DefaultLayout = Backbone.Marionette.Layout.extend({
       var fullScreenMap = document.getElementById('full-screen-map');
       if (fullScreenMap) {
         fullScreenMap.parentNode.removeChild(fullScreenMap);
+        window.mapFullScreen = false;
       }
     }
   }

app/scripts/utilities.js
@@ -37,13 +37,15 @@ $.fn.serializeObject = function()
 
 window.toggleFullScreen = function () {
     if (window.mapFullScreen) {
+        // full screen -> normal screen
         var fullScreenMap = document.getElementById('full-screen-map');
         fullScreenMap.parentNode.removeChild(fullScreenMap); //remove()
         var locationSearch = document.getElementById('geocoderRegion');
         locationSearch.style.cssText = "";
 
-        var header = document.getElementById('header');
-        header.classList.add('fullscreen');
+        // Is a different styling needed in normal mode?
+        //var header = document.getElementById('header');
+        //header.classList.remove('fullscreen');
 
         Lizard.mapView.fullScreenRegion.close();
         var lonlatzoom = window.location.hash.split('#map/')[1];
@@ -65,6 +67,7 @@ window.toggleFullScreen = function () {
             map: leafletView
           });
     } else {
+        // normal screen -> full screen
         var fullScreenMap = document.getElementById('full-screen-map');
         if (fullScreenMap === null) {
             fullScreenMap = document.createElement('div');
@@ -73,8 +76,9 @@ window.toggleFullScreen = function () {
             container.parentNode.insertBefore(fullScreenMap, container);
         }
 
-        var header = document.getElementById('header');
-        header.classList.remove('fullscreen');
+        // Is a different styling needed in full-screen mode?
+        //var header = document.getElementById('header');
+        //header.classList.add('fullscreen');
         fullScreenMap.style.cssText = "position: absolute; width: 100%; height: calc(100% - 40px); top: 40px;";
         var locationSearch = document.getElementById('geocoderRegion');
         locationSearch.style.cssText = "margin-top: calc(75%);"

app/styles/main.css
@@ -45,11 +45,15 @@ iframe {
   width: 100%;
 }
 
-#header.fullscreen {
+#header {
     height: 40px !important;
     overflow: hidden;
     display:block ;
     z-index: 10;
+}
+
+#header.fullscreen {
+    /* Is a different styling needed in full-screen mode? */
 }
 
 span#brand {