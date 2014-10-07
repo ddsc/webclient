e6494ceb0db43b19ce0b80c2687032a83fc0e1e1
fritzvd <f.vandeventer@gmail.com>
2014-10-07 08:41:22 +0200

    collages work with associated models again

--- app/scripts/lizard/models/Collage.js
+++ app/scripts/lizard/models/Collage.js
@@ -1,9 +1,18 @@
-Lizard.Models.Collage = Backbone.AssociatedModel.extend({
+Lizard.Models.Collage = Backbone.Model.extend({
   relations: [{
     type: Backbone.Many, //nature of the relationship
     key: 'collageitems', //attribute of collage relating to workspaceItems
-    relatedModel: Lizard.Models.CollageItem //AssociatedModel for attribute key
+    relatedModel: 'Lizard.Models.CollageItem' //AssociatedModel for attribute key
   }],
+  initialize: function (model) {
+    var collageitems = [];
+    for (var i = 0; i < model.collageitems.length; i++) {
+      var collageitem = new Lizard.Models.CollageItem(model.collageitems[i]);
+      collageitems.push(collageitem);
+    }
+    var newCollection = new Backbone.Collection(collageitems);
+    this.set('collageitems', newCollection);
+  },
   defaults: {
     name: '',
     id: null,
