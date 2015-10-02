9fcda42e669eb41b39a6dd586f5a666625a1e8c1
Carsten Byrman <carsten.byrman@nelen-schuurmans.nl>
2015-10-02 15:07:32 +0200

    Filter out timeseries that cannot be displayed as a graph.

--- app/scripts/lizard/collections/Timeseries.js
+++ app/scripts/lizard/collections/Timeseries.js
@@ -2,7 +2,9 @@
 
 Lizard.Collections.InfiniteTimeseries = Backbone.Collection.extend({
   url: function() {
-    return settings.timeseries_url + '?ordering=name&page_size=' + this.pageSize + '&page=' + this.page + '&search=' + this.query;
+    // Only return timeseries of type integer or float.
+    // NB: value_type__in doesn't work nor does value_type__lt=2 (2 isn't a valid value_type).
+    return settings.timeseries_url + '?value_type__lte=1&ordering=name&page_size=' + this.pageSize + '&page=' + this.page + '&search=' + this.query;
   },
   // COMPARATOR DOES NOT WORK WELL HERE, MUST HAVE PROPER ORDERING ON SERVER RETURN?
   parse: function(resp, xhr) {
