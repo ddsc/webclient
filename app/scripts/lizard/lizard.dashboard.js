faf42e0df94fa60768fcff6d1bf9a4e627c633b7
Carsten Byrman <carsten.byrman@nelen-schuurmans.nl>
2015-12-21 12:02:10 +0100

    Annotate OMZD UUIDs.

--- app/scripts/lizard/lizard.dashboard.js
+++ app/scripts/lizard/lizard.dashboard.js
@@ -1,13 +1,13 @@
 Lizard.Dashboard = {};
 
 Lizard.Dashboard.omzdDefinition = {
-    debiet_l: 'd856946f-feb6-4536-a878-af55c32cd96c',
-    debiet_r: 'dd7a4db5-4261-4ac4-9583-0572e7f39f66',
-    buitendruk: '5c8d7780-14c8-4237-9bca-73849e7990b7',
-    temp_l: '29119285-2428-4863-b630-9b5aa095de13',
-    temp_r: '03073703-3509-40f2-ba89-d85361c4021b',
-    waterst_r: '49108710-6c58-4e31-ac2d-3c4933040439',
-    waterst_l: '9dfdc3f7-88f7-4ff0-a671-b4094ac12b7a',
+    debiet_l: 'd856946f-feb6-4536-a878-af55c32cd96c',  // Actueel Debiet Dijk Links
+    debiet_r: 'dd7a4db5-4261-4ac4-9583-0572e7f39f66',  // Actueel Debiet Dijk Rechts
+    buitendruk: '5c8d7780-14c8-4237-9bca-73849e7990b7',  // Buitendruk in kPa
+    temp_l: '29119285-2428-4863-b630-9b5aa095de13',  // Temperatuur Dijk Links
+    temp_r: '03073703-3509-40f2-ba89-d85361c4021b',  // Temperatuur Dijk Rechts
+    waterst_r: '49108710-6c58-4e31-ac2d-3c4933040439',  // Waterkolom dijk links in mNAP
+    waterst_l: '9dfdc3f7-88f7-4ff0-a671-b4094ac12b7a',  // Waterkolom dijk rechts in mNAP
     activeDashboard: 'omzd'
 };
 
